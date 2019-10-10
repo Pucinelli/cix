4b234b36d0b674d4f9075b12f16f22f2c1136364
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-10-10 15:05:45 -0300

    Create file operations

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'sync',
   alias: 's',
   run: async toolbox => {
-    const { print, parameters, filesystem } = toolbox
+    const { print, parameters, operations } = toolbox
 
     if (!parameters.first) {
       print.error('Error: Project token not specified')
@@ -11,124 +11,17 @@ module.exports = {
 
     const io = require('socket.io-client');
     const client = io('http://localhost:8080');
-  
-    client.emit('cix:startup')
-
-    const STATUS = { OK: 0, ERR: 1 }
-    const verbose = (msg, mode = 'info') => {
-      if (parameters.options.verbose || parameters.options.v)
-        print[mode](msg)
-    }
-    const reply = (client, data) => {
-      verbose('Replying status to server')
-      client.emit('cix:status', data)
-    }
     
     client.on('server:fetchProjectData', data => {
-      print.info(`Handling data: ${JSON.stringify(data, null, 4)}`)
-      reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:fetchProjectData' })
+      print.info(`Server sent: ${JSON.stringify(data, null, 4)}`)
     })
-
     
-    client.on('server:createFile', data => {
-      const { join } = require('path')
-      const { path, filename, isDirectory } = data
+    client.on('server:createFile', operations.createFile)
+    client.on('server:writeFile', operations.writeFile)
+    client.on('server:removeFile', operations.removeFile)
+    client.on('server:moveFile', operations.moveFile)
+    client.on('server:shellCommand', operations.shellCommand)
 
-      verbose(`Creating file '${filename}'`)
-      const file = isDirectory
-        ? filesystem.dirAsync(join(path, filename))
-        : filesystem.fileAsync(join(path, filename + data.extension))
-      
-      file.then(res => {
-        verbose('File created successfully', 'success')
-        reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:createFile' })
-      }).catch(err => {
-        print.error('Error creating file')
-        print.info(err)
-        reply(client, { projectId: parameters.second, status: STATUS.ERR, req: 'server:createFile' })
-      })
-    })
-
-    
-    client.on('server:removeFile', data => {
-      const { join } = require('path')
-      const { path, filename, isDirectory } = data
-
-      verbose(`Removing file '${filename}'`)
-      const file = isDirectory
-        ? filesystem.removeAsync(join(path, filename))
-        : filesystem.removeAsync(join(path, filename + data.extension), data.content)
-      
-      file.then(res => {
-        verbose('File removed successfully', 'success')
-        reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:removeFile' })
-      }).catch(err => {
-        print.error('Error creating file')
-        print.info(err)
-        reply(client, { projectId: parameters.second, status: STATUS.ERR, req: 'server:removeFile' })
-      })
-    })
-
-    
-    client.on('server:moveFile', data => {
-      const { from, to } = data
-      
-      verbose(`Moving file from '${from}' to '${to}'`)
-      filesystem.moveAsync(from, to)
-        .then(res => {
-          verbose('Success moving file', 'success')
-          reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:moveFile' })
-        }).catch(err => {
-          print.error('Error moving file')
-          print.info(err)
-          reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:moveFile' })
-        })
-    })
-
-
-    client.on('server:writeFile', data => {
-      const { join } = require('path')
-      const { path, filename, extension, content } = data
-
-      verbose(`Writing on file '${filename}'`)
-      filesystem.writeAsync(join(path, filename + extension), content)
-        .then(res => {
-          verbose('Wrote on file successfully', 'success')
-          reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:writeFile' })
-        }).catch(err => {
-          print.error('Error writing on file')
-          print.info(err)
-          reply(client, { projectId: parameters.second, status: STATUS.OK, req: 'server:writeFile' })
-        })
-    })
-
-
-    const shcmd = {}
-    client.on('server:shellCommand', data => {
-      const { spawn } = require('child_process')
-      const { command, params, pid } = data
-
-      if (!!shcmd[pid]) { // commands to a running proccess
-        const cmd = shcmd[pid]
-        cmd.stdin.write(data.input)
-      } else {
-        verbose(`Starting '${command}'`)
-
-        const cmd = spawn(command, params)
-        shcmd[pid] = cmd
-
-        cmd.stdout.on('data', data => {
-          client.emit('cix:shellOutput', { projectId, pid, data })
-        })
-        
-        cmd.stderr.on('data', data => {
-          client.emit('cix:shellOutput', { projectId, pid, data })
-        })
-        
-        cmd.on('close', code => {
-          reply(client, { projectId: parameters.second, status: code, req: 'server:shellCommand' })
-        })
-      }
-    })
+    client.emit('cix:startup')
   }
 }
--- src/extensions/operations.js
+++ src/extensions/operations.js
@@ -1,38 +1,77 @@
 module.exports = toolbox => {
   
+  // probably gonna rename this to something fs-related
   toolbox.operations = {
     createFile: async data => {
       const { 
-        isDirectory, path, name, ext
+        isDirectory, path, filename, extension = ''
       } = data
       const { filesystem } = toolbox
       const { join } = require('path')
-      const fullFilePath = join(path, name)
+      const fullFilePath = join(path, filename)
       
       if (isDirectory)
         await filesystem.dirAsync(fullFilePath)
       else
-        await filesystem.fileAsync(fullFilePath + ext)
+        await filesystem.fileAsync(fullFilePath + extension)
     },
   
     
     writeFile: async data => {
+      const { 
+        content, path, filename, extension = ''
+      } = data
+      const { filesystem } = toolbox
+      const { join } = require('path')
+      const fullFilePath = join(path, filename)
       
+      await filesystem.writeAsync(fullFilePath + extension, content)
     },
   
     
     removeFile: async data => {
+      const { 
+        path, filename, extension = ''
+      } = data
+      const { filesystem } = toolbox
+      const { join } = require('path')
+      const fullFilePath = join(path, filename)
       
+      await filesystem.removeAsync(fullFilePath + extension)
     },
   
     
     moveFile: async data => {
+      const { from, to } = data
+      const { filesystem } = toolbox
       
+      await filesystem.moveAsync(from, to)
     },
   
-    
+    // probably gonna move this to other file
     shellCommand: async data => {
+      const { spawn } = require('child_process')
+      const { command, parameters } = data
+
+      if (processStillRunning) {
+        const process = getProcessSomehow()
+        process.stdin.write(data.input)
+
+      } else {
+        const process = spawn(command, parameters)
       
+        process.stdout.on('data', data => {
+          
+        })
+        
+        process.stderr.on('data', data => {
+          
+        })
+        
+        process.on('close', code => {
+          
+        })
+      }
     }
   }
 }
