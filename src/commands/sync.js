031ffd1a5abe720d582b1cd8cb01ecae7e989428
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-10-11 16:44:17 -0300

    Add basic message handling in 'sync' command

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -12,15 +12,70 @@ module.exports = {
     const io = require('socket.io-client');
     const client = io('http://localhost:8080');
     
-    client.on('server:fetchProjectData', data => {
-      print.info(`Server sent: ${JSON.stringify(data, null, 4)}`)
+    client.on('server:finish', () => client.close())
+
+    client.on('server:fetchProjectData', async data => {
+      print.info('Fetching project data')
+      for (let file of data.files) {
+        const filename = file.path.join('/') + '/' + file.filename + file.extension
+        print.info(`Creating file ${filename}`)
+        operations.createFile(file)
+      }
+      client.emit('cix:status', { req: 'server:fetchProjectData', status: 0 })
     })
     
-    client.on('server:createFile', operations.createFile)
-    client.on('server:writeFile', operations.writeFile)
-    client.on('server:removeFile', operations.removeFile)
-    client.on('server:moveFile', operations.moveFile)
-    client.on('server:shellCommand', operations.shellCommand)
+    client.on('server:createFile', async data => {
+      print.info('Creating files')
+      for (let file of data) {
+        const filename = file.path.join('/') + '/' + file.filename + file.extension
+        print.info(`Creating file ${filename}`)
+        operations.createFile(file)
+      }
+      client.emit('cix:status', { req: 'server:createFile', status: 0 })
+    })
+    
+    client.on('server:writeFile', async data => {
+      print.info('Writing files')
+      for (let file of data) {
+        const filename = file.path.join('/') + '/' + file.filename + file.extension
+        print.info(`Writing file ${filename}`)
+        operations.writeFile(file)
+      }
+      client.emit('cix:status', { req: 'server:writeFile', status: 0 })
+    })
+    
+    client.on('server:removeFile', async data => {
+      print.info('Removing files')
+      for (let file of data) {
+        const filename = file.path.join('/') + '/' + file.filename + file.extension
+        print.info(`Removing file ${filename}`)
+        operations.removeFile(file)
+      }
+      client.emit('cix:status', { req: 'server:removeFile', status: 0 })
+    })
+
+    client.on('server:moveFile', async data => {
+      print.info('Moving files')
+      for (let { from, to } of data) {
+        const fromFilename = from.path.join('/') + '/' + from.filename + from.extension
+        const toFilename = to.path.join('/') + '/' + to.filename + to.extension
+        print.info(`Moving file from ${fromFilename} to ${toFilename}`)
+        operations.moveFile({ from, to })
+      }
+      client.emit('cix:status', { req: 'server:moveFile', status: 0 })
+    })
+
+    client.on('server:shellCommand', async data => {
+      const { type, command, parameters } = data
+      print.info('Running shell command')
+      if (type === 'spawn') {
+        print.info(`Spawning ${command} command with ${parameters} parameters`)
+        this.runningCommand = operations.shellCommand({ client, command, parameters })
+      } else {
+        print.info(`Sending ${parameters} as input to ${command} command`)
+        this.runningCommand.stdin.write(command)
+      }
+    })
 
     client.emit('cix:startup')
   }
