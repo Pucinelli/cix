7cb35cadf294357946c26655186bfbfa20563385
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-12-01 00:07:04 -0300

    Update toolbox crdt behavior

--- src/extensions/crdt.js
+++ src/extensions/crdt.js
@@ -1,32 +1,12 @@
 module.exports = toolbox => {
 
   toolbox.crdt = {
-    writeAsync: async data => {
-      const { file, content } = data
-      const { filesystem } = toolbox
-      const { KSeq } = require('../config/kseq/index')
-      
-      const previousContent = await filesystem.existsAsync(file)
-        ? await filesystem.readAsync(file)
-        : ''
-      
-      const kseq = new KSeq()
-      
-      for (let i = 0; i < previousContent.length; i++)
-        kseq.append(previousContent[i])
+    writeAsync: async filename => {
+      const { filesystem, projectState } = toolbox
 
-      for (let { type, data, pos } of content) {
-        // console.log({ type, data, pos })
-        if (type === '+')
-          kseq.insert(data, pos)
-        else
-          kseq.remove(pos)
-      }
+      const contentBuilt = projectState[filename].crdt.toArray().join('')
 
-      const contentBuilt = kseq.toArray().join('')
-      await filesystem.writeAsync(file, contentBuilt)
-      //console.log(contentBuilt)
-      
+      return await filesystem.writeAsync(filename, contentBuilt)
     }
   }
 
