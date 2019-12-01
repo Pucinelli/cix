241956dacab9a5ef3fca930db4a5133b638d4cba
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-12-01 00:09:02 -0300

    Make every functionality work

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -4,63 +4,126 @@ module.exports = {
   description: 'Start sync process of cix with the default platform',
   run: async toolbox => {
     const { print, parameters, operations } = toolbox
+    const jwt = require('jwt-simple')
+    const { KSeq } = require('../config/kseq/index')
 
     if (!parameters.first) {
       print.error('Error: Project token not specified')
       return
     }
 
+    try {
+      const payload = jwt.decode(parameters.first, 'cix-is-awesome')
+      toolbox.projectConfig = await operations.initProject(payload)
+    } catch (err) {
+      print.error('Error: Could not parse token')
+      console.log(err)
+      return
+    }
+
+    if (!toolbox.projectConfig) {
+      print.error('Error: Could not load cix configuration')
+      return
+    }
+
     const io = require('socket.io-client');
-    const client = io('http://localhost:8080');
-    
-    client.childProcesses = {}
+    const client = io('http://localhost:3000');
 
-    client.on('server:finish', () => client.close())
+    // DONE
+    client.on('server:finish', message => message
+      ? print.info(message) || client.close()
+      : client.close())
 
+    // DONE
     client.on('server:fetchProjectData', async data => {
       print.info('Fetching project data')
-      for (let file of data.files)
+
+      toolbox.projectState = {}
+
+      const formatFile = (fileTree, path = '') => {
+        fileTree.forEach(file => {
+          file.path = path ? `${path}/${file.name}` : file.name
+          if ('children' in file)
+            formatFile(file.children, file.path)
+          else {
+            file.crdt = new KSeq(file.path)
+            toolbox.projectState[file.path] = file
+          }
+        })
+      }
+
+      formatFile(data)
+
+      for (let file of Object.values(toolbox.projectState))
         await operations.createFile(file)
+
       client.emit('cix:status', { req: 'server:fetchProjectData', status: 0 })
     })
-    
+
+    // DONE
     client.on('server:createFile', async data => {
       print.info('Creating files')
-      for (let file of data)
-        await operations.createFile(file)
+
+      await operations.createFile(data)
       client.emit('cix:status', { req: 'server:createFile', status: 0 })
     })
-    
-    client.on('server:writeFile', async data => {
-      print.info('Writing files')
-      for (let file of data)
-        await operations.writeFile(file)
-      client.emit('cix:status', { req: 'server:writeFile', status: 0 })
-    })
-    
+
+    // DONE
     client.on('server:removeFile', async data => {
       print.info('Removing files')
-      for (let file of data)
-        await operations.removeFile(file)
+
+      await operations.removeFile(data)
       client.emit('cix:status', { req: 'server:removeFile', status: 0 })
     })
 
+    // DONE
+    client.on('server:writeFile', async data => {
+      const file = toolbox.projectState[data.file]
+
+      print.info('Writing files')
+
+      if (data.type === '+') {
+        file.content.s.push([`${data.pos}#`, data.change])
+        file.crdt.insert(data.change, data.pos - 1)
+      } else {
+        file.content.r.push(data.pos)
+        file.crdt.remove(data.pos)
+      }
+
+      await operations.writeFile(data.file)
+      client.emit('cix:status', { req: 'server:writeFile', status: 0 })
+    })
+
+    // DONE
     client.on('server:moveFile', async data => {
       print.info('Moving files')
-      for (let { from, to } of data)
-        await operations.moveFile({ from, to })
+      await operations.moveFile(data)
       client.emit('cix:status', { req: 'server:moveFile', status: 0 })
     })
 
+    // DONE
     client.on('server:shellCommand', async data => {
-      const { type, command, parameters, pid } = data
-      print.info('Running shell command')
-      if (type === 'spawn')
-        await operations.shellCommand({ client, command, parameters, processes: client.childProcesses })
-      else
-        client.childProcesses[pid].stdin.write(command)
+      const { childProcess } = toolbox
+
+      if (toolbox.runningProcess) {
+        toolbox.runningProcess.stdin.write(data + '\n')
+      } else {
+        print.info('Running shell command')
+        childProcess.run(data, [], {
+          stdout(output) {
+            client.emit('cix:status', { req: 'server:shellCommand', output: output.toString() })
+          },
+          stderr(output) {
+            client.emit('cix:status', { req: 'server:shellCommand', output: output.toString() })
+          },
+          close(statusCode) {
+            client.emit('cix:status', { req: 'server:shellCommand', statusCode })
+          },
+        })
+      }
     })
 
-    client.emit('cix:startup')
+    // DONE
+    client.emit('cix:startup', parameters.first)
   }
 }
